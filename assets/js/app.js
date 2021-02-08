8457a52dabcfe2bae98ac17f71adcc2ccb2abd1f
KLDudzinsky <kldudzinsky@gmail.com>
2021-02-07 17:00:34 -0800

    finished base assignment

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,13 +21,13 @@ var height = svgHeight - margin.top - margin.bottom;
 
 //create SVG Wrapper
 var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    .select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 //initial params
 var chosenXAxis = "income";
 //scale
@@ -39,18 +39,14 @@ function xScale (data){
         .range([0,width])
     return xLinearScale
 };
-//function update x-scale var upon click
-//function update xAxis var upon click
-//function update circles group with transition to new circles
-//function used for updating circles group w new toolTip
- //load csv 
+//load and read CSV 
 d3.csv("data.csv").then(function(data){
     //if (err) throw err; 
     //format
         data.forEach(function(data){
             data.income = +data.income;
             data.age=+data.age;
-            //data.poverty=+data.poverty;
+
         });
     
     //xLinearScale function above CSV import
@@ -59,20 +55,11 @@ d3.csv("data.csv").then(function(data){
         var yLinearScaleAge= d3.scaleLinear()
             .domain([20,d3.max(data, d=>d.age)])
             .range([height, 0]);
-        // var yLinearScalePoverty=d3.scaleLinear()
-        //     .domain([0,d3.max(data, d=>d.poverty)])
-        //     .range([height,0]);
+       
     //create initial axis functions
         var bottomAxis = d3.axisBottom(xLinearScale);
         var leftAxisAge = d3.axisLeft(yLinearScaleAge);
-        //var leftAxisPoverty = d3.axisLeft(yLinearScalePoverty);
-    //configure line function to plot coordinates
-    // var LineA = d3.line()
-    //     .x(data => xLinearScale(data.income))
-    //     .y(data => yLinearScaleAge(data.age));
-            // var LineP= d3.line()
-            //     .x(data => xLinearScale(data.income))
-            //     .y(data => yLinearScaleAge(data.poverty));
+    
     //append x Axis
     var xAxis = chartGroup.append("g")
         //.classed("active", true)
@@ -81,38 +68,34 @@ d3.csv("data.csv").then(function(data){
     //append y axes
     var YAAxis = chartGroup.append("g")
         .call(leftAxisAge);
-            // var YPAxis = chartGroup.append("g")
-            //     .call(leftAxisPoverty);
+    
     //append initial circles
     var circlesGroup = chartGroup.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
-        //.text(function(d){return d.age;})
         .attr("cx", d=> xLinearScale(d.income))
         .attr("cy", d=> yLinearScaleAge(d.age))
         .attr("r",20)
         .attr("fill", "blue")
         .attr("opacity", ".5")
-        
-   });
+     });
 //axes labels
    var labelsGroup = chartGroup.append("g")
     .attr("transform", `translate(${width / 2}, ${height + 20})`);
-    //age y label
 
-    //income x label
+    //age label
     chartGroup.append("text")
         .attr("transform", "rotate(-90)") 
         .attr("y", 0 - margin.left)
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
-        .classed("atext", true)
-        .text("Number of Billboard 500 Hits");
-    //poverty y label
-
+        .attr("value", "age")
+        .text("Age");
+    //income label
+    var incomeLable = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("value", "income") 
+    .text("Income");
     
-//to do : TEXT 
-
-   //tooltips event handler on circlesgroup.on mouseover, click, mouseoff 
-   //
